30482b824b431b3390b8af87c6934fe727790ba8
Burak Karakan <burak.karakan@gmail.com>
2018-03-28 14:45:53 +0300

    Moved reminder buttom to the date row.

--- js/Components/AssignmentRow/DateRow.js
+++ js/Components/AssignmentRow/DateRow.js
@@ -1,32 +1,105 @@
 import BaseDomElement from "../BaseDomElement.js";
+import Database from "../../Database.js";
 
 export default class extends BaseDomElement {
   constructor(document, startDate, endDate) {
     super(document);
     this.startDate = startDate;
     this.endDate = endDate;
+    this.db = new Database();
   }
 
   render() {
     const icon = this.icon.regular('calendar-alt');
     icon.classList.add('date-row-icon');
+    const iconCol = this.newCol(1);
+    iconCol.appendChild(icon);
 
-    const startDateDiv = this.newDiv('date');
-    startDateDiv.textContent = this.startDate;
-
-    const endDateDiv = this.newDiv('date');
+    const endDateDiv = this.newCol(6);
+    endDateDiv.classList.add('date');
     endDateDiv.textContent = this.endDate;
 
-    const dateSeperator = this.newDiv('date-seperator');
-    dateSeperator.textContent = ' - ';
+    const buttonCol = this.newCol(7);
+    const button = this.renderSetterButton();
+    buttonCol.appendChild(button);
+    buttonCol.id = 'reminder-button-wrapper';
 
     const row = this.newDiv('row');
     row.classList.add('text-sm', 'date-row');
-    row.appendChild(icon);
-    row.appendChild(startDateDiv);
-    row.appendChild(dateSeperator);
+    row.appendChild(iconCol);
     row.appendChild(endDateDiv);
+    row.appendChild(buttonCol);
 
     return row;
+  }
+
+
+  /**
+   * Create and return a reminder setter button instance.
+   */
+  renderSetterButton() {
+    const button = this._buttonRenderer(true);
+    button.classList.add('set-reminder', 'pulse');    
+    return button;
+  }
+
+  /**
+   * Create and return a reminder remover button instance.
+   */
+  renderRemoverButton() {
+    const button = this._buttonRenderer(false);
+    button.classList.add('remove-reminder');
+    return button;
+  }
+
+  /**
+   * 
+   * @param {bool} isSetter 
+   */
+  _buttonRenderer(isSetter) {
+    const icon = isSetter ? this.icon.solid('bell') : this.icon.regular('bell');
+    const remind = this.newButton('btn-reminder');
+    remind.classList.add('pull-right', 'btn');
+    remind.addEventListener('click', isSetter ? this.setReminders.bind(this) : this.removeReminders.bind(this));
+    remind.appendChild(icon);
+
+    const span = this.newDiv('btn-msg');
+    span.textContent = isSetter ? 'Set Reminders' : 'Remove Reminders';
+    remind.appendChild(span);
+
+    return remind;
+  }
+
+  /**
+   * Set reminders for this assignment.
+   * @param {*} event 
+   */
+  setReminders(event) {
+    const target = event.target.parentNode;
+    const targetParent = this._getButtonWrapper();
+
+    const newButton = this.renderRemoverButton();
+    targetParent.replaceChild(newButton, target);
+
+    this.db.get(this.assignmentLink, (assignment) => {
+      const reminder = new ReminderSetter(Object.values(assignment)[0]);
+      reminder.set();
+    })
+  }
+
+  /**
+   * Remove the reminders that are set for this assignment.
+   * @param {*} event 
+   */
+  removeReminders(event) {
+    const target = event.target.parentNode;
+    const targetParent = this._getButtonWrapper();
+
+    const newButton = this.renderSetterButton();
+    targetParent.replaceChild(newButton, target);
+  }
+
+  _getButtonWrapper(){
+    return this.document.getElementById('reminder-button-wrapper');
   }
 }
--- js/Components/AssignmentRow/NameRow.js
+++ js/Components/AssignmentRow/NameRow.js
@@ -17,76 +17,14 @@ export default class extends BaseDomElement {
 
     // Create the link and col contents.
     const a = this.newLink('link-underline', this.assignmentLink, this.assignmentName);
-    const nameCol = this.newCol(10);
+    const nameCol = this.newCol(12);
     nameCol.appendChild(a);
-
-    // Create the reminder button column.
-    const remindButton = this.setReminderButton();
-    const remindCol = this.newCol(2);
-    remindCol.classList.add('pull-right');
-    remindCol.appendChild(remindButton);
 
     // Append the col to a new row.
     const row = this.newDiv('row');
     row.classList.add('assignment-name');
     row.appendChild(nameCol);
-    row.appendChild(remindCol);
 
     return row;
-  }
-
-  /**
-   * Create and return a reminder setter button instance.
-   */
-  setReminderButton() {
-    const icon = this.icon.regular('bell');
-    const remind = this.newButton('set-reminder');
-    remind.classList.add('pull-right');
-    remind.addEventListener('click', this.setReminders.bind(this));
-    remind.appendChild(icon);
-
-    return remind;
-  }
-
-  /**
-   * Create and return a reminder remover button instance.
-   */
-  removeReminderButton() {
-    const icon = this.icon.solid('bell');
-    const remind = this.newButton('remove-reminder');
-    remind.classList.add('pull-right');
-    remind.addEventListener('click', this.removeReminders.bind(this));
-    remind.appendChild(icon);
-
-    return remind;
-  }
-
-  /**
-   * Set reminders for this assignment.
-   * @param {*} event 
-   */
-  setReminders(event) {
-    const target = event.target.parentNode;
-    const targetParent = target.parentNode;
-
-    const newButton = this.removeReminderButton();
-    targetParent.replaceChild(newButton, target);
-
-    this.db.get(this.assignmentLink, (assignment) => {
-      const reminder = new ReminderSetter(Object.values(assignment)[0]);
-      reminder.set();
-    })
-  }
-
-  /**
-   * Remove the reminders that are set for this assignment.
-   * @param {*} event 
-   */
-  removeReminders(event) {
-    const target = event.target.parentNode;
-    const targetParent = target.parentNode;
-
-    const newButton = this.setReminderButton();
-    targetParent.replaceChild(newButton, target);
   }
 }
